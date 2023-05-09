1c513fdd324754d0b986ed78ee8820a00397d783
Rafael Ribon <ribon.rafael@gmail.com>
2023-05-09 17:12:18 -0300

    Update runner-electron.js

--- runner-electron.js
+++ runner-electron.js
@@ -2,47 +2,29 @@ const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
 
-// cypress.run({
-//   // specs to run here
-//   autoCancelAfterFailures: 2,
-// //   testingType: "e2e"
+cypress.run({
+  // specs to run here
+  autoCancelAfterFailures: 2,
+  testingType: "e2e",
   
 
-// })
-// .then((results) => {
-  
+})
+.then((results) => {
+ 
+  console.log("results.totalFailed", results.totalFailed),
 
-//   console.log("Testes", results.totalFailed),
-//   if(results.totalFailed = 1) {
-//     console.log("Teste")
+  
+//   const args = {
+//     target: process.env.TARGET_TOKEN_ELECTRON,
 //   }
-  
-// //   const args = {
-// //     target: process.env.TARGET_TOKEN_ELECTRON,
-// //   }
 
   
-// //   tesults.results(results, args);
-// })
-// .catch((err) => {
-//  console.error(err)
-// })
-
-cypress.run({...})
-.then(result => {
-  if (result.failures) {
-    console.error('Could not execute tests')
-    console.error(result.message)
-    process.exit(result.failures)
-  }
-
-  // print test results and exit
-  // with the number of failed tests as exit code
-  process.exit(result.totalFailed)
+//   tesults.results(results, args);
 })
-.catch(err => {
-  console.error(err.message)
-  process.exit(1)
+.catch((err) => {
+ console.error(err)
 })
+
+
 
 
