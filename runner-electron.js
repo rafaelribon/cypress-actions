1d47bfe190af6307bb4dd7ea61a1a1498dea9af9
Rafael Ribon <ribon.rafael@gmail.com>
2023-05-09 16:04:17 -0300

    Update runner-electron.js

--- runner-electron.js
+++ runner-electron.js
@@ -4,31 +4,18 @@ const tesults = require('cypress-tesults-reporter');
 
 cypress.run({
   // specs to run here
+  autoCancelAfterFailures: 1
+  
 
 })
-.then(result => {
-  if (result.failures) {
-    
-    const args = {
+.then((results) => {
+  const args = {
     target: process.env.TARGET_TOKEN_ELECTRON,
   }
-    
-    console.error('Could not execute tests')
-    console.error(result.message)
-    process.exit(result.failures)
-  }
-  
-
-  // print test results and exit
-  // with the number of failed tests as exit code
   tesults.results(results, args);
-  process.exit(result.totalFailed)
-  
+})
+.catch((err) => {
+ console.error(err)
 })
 
 
-
-.catch(err => {
-  console.error(err.message)
-  process.exit(1)
-})
